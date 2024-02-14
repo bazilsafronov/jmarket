079a5fe63ad3c3917c1c8de88250fb3005f0ceaf
Bazil <bazil.safronov@gmail.com>
2024-02-14 23:13:06 +0300

    add sass

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,10 @@
+import styles from './Card.module.scss'
+
+
+const Card = () => {
+  return (
+    <div className={styles.card}>Card</div>
+  )
+}
+
+export default Card
--- src/views/Catalog/Catalog.tsx
+++ src/views/Catalog/Catalog.tsx
@@ -1,9 +1,20 @@
-import styles from './Catalog.module.sass';
+import Card from '../../components/Card/Card';
+import Header from '../../components/Header/Header';
+import styles from './Catalog.module.scss';
 
 
 const Catalog = () => {
   return (
-    <div>Catalog</div>
+    <>
+    <Header />
+    <div className={styles.catalog}>
+    <div><Card /></div>
+    <div><Card /></div>
+    <div><Card /></div>
+    <div><Card /></div>
+    </div>
+   
+    </>
   )
 }
 
